702417a5066c3f54fc1f12e88aec8468d46d8e07
Sami Asfouri <35670834+DoubleStackedMind@users.noreply.github.com>
2018-12-04 15:34:42 +0100

    04/12/2018 v2

--- app/src/main/java/com/android/esprit/smartreminders/Fragments/TaskFragment.java
+++ app/src/main/java/com/android/esprit/smartreminders/Fragments/TaskFragment.java
@@ -1,25 +1,59 @@
 package com.android.esprit.smartreminders.Fragments;
 
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.text.format.DateFormat;
 
 import com.android.esprit.smartreminders.R;
+
+import java.sql.Time;
+import java.util.Calendar;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class TaskFragment extends Fragment {
+public class TaskFragment extends Fragment implements TimePickerDialog.OnTimeSetListener, View.OnClickListener {
 
+    int hour,minute;
+    int hourFinal, minuteFinal;
+    Button Timebtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_task, container, false);
+        View view = inflater.inflate(R.layout.fragment_task, container, false);
+        Timebtn = view.findViewById(R.id.TimeBtn);
+        Timebtn.setOnClickListener(this);
+        return view;
+    }
+
+
+    @Override
+    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+        Timebtn.setText(hourOfDay + ":" +minute);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+     if(view.getId() == Timebtn.getId()) {
+         Calendar c = Calendar.getInstance();
+         hour = c.get(Calendar.HOUR_OF_DAY);
+         minute = c.get(Calendar.MINUTE);
+
+         TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(), this, hour, minute, DateFormat.is24HourFormat(getActivity()));
+         timePickerDialog.show();
+     }
     }
 
 }
