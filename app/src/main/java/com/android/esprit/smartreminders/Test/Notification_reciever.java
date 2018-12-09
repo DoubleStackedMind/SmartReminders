7cbb50a161ffa9e3e68b2ba56671dce5bf1a0d3f
Sami Asfouri <35670834+DoubleStackedMind@users.noreply.github.com>
2018-12-09 17:50:21 +0100

    Commit Event + Mute v1.0

--- app/src/main/java/com/android/esprit/smartreminders/Test/Notification_reciever.java
+++ app/src/main/java/com/android/esprit/smartreminders/Test/Notification_reciever.java
@@ -1,20 +1,29 @@
 package com.android.esprit.smartreminders.Test;
 
-import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
-import android.widget.EditText;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+
+import com.android.esprit.smartreminders.Fragments.FragmentFormEvent;
+
 
 public class Notification_reciever extends BroadcastReceiver {
+
+    Camera camera;
+    Parameters parameters;
 
     @Override
     public void onReceive(Context context, Intent intent) {
         NotificationHelper notificationHelper = new NotificationHelper(context);
-        NotificationCompat.Builder nb = notificationHelper.getChannelNotification("",
-                "");
-        notificationHelper.getManager().notify(1,nb.build());
+        NotificationCompat.Builder nb = notificationHelper.getChannelNotification("", "");
+        notificationHelper.getManager().notify(1, nb.build());
+        FragmentFormEvent f = new FragmentFormEvent();
 
     }
-}
+
+    }
+
+
